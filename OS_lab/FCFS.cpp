540df8ef7a27e4454e4579bd6073f63e3b980457
snigdha181 <132484067+snigdha181@users.noreply.github.com>
2023-10-31 11:59:45 +0600

    Add files via upload

--- OS_lab/FCFS.cpp
+++ OS_lab/FCFS.cpp
@@ -5,20 +5,39 @@ int main()
     int n;
     cout<<"Enter the number of process: ";
     cin>>n;
-    int burst_time[n];
+    int bt[n],art[n];
     for(int i=0; i<n; i++)
     {
-        cout<<"Enter the burst time of process - "<<i+1<<" : ";
-        cin>>burst_time[i];
+        cout<<"Enter the burst time of process"<<i+1<<": ";
+        cin>>bt[i];
+        cout<<"Enter the arrival time of process"<<i+1<<": ";
+        cin>>art[i];
     }
-    int waiting_time=0;
-    int total_waiting_time=0;
+
     for(int i=0; i<n; i++)
     {
-        cout<<"Waiting time for process "<<i+1<<" : "<<waiting_time<<endl;
-        waiting_time= waiting_time+burst_time[i];
-        total_waiting_time = total_waiting_time + waiting_time;
+        for(int j=i+1; j<n; j++)
+        {
+            if(art[i]>art[j])
+            {
+                int temp=art[i];
+                art[i]=art[j];
+                art[j]=temp;
+
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+            }
+        }
     }
-    cout<<"Average waiting time: "<<(total_waiting_time - waiting_time)/float(n);
+    int wt=0,tt=0;
+    cout<<"Process\t\t Waiting time\n";
+    for(int i=0; i<n; i++)
+    {
+        cout<<i+1<<"\t\t"<<wt<<endl;
+        wt=wt+bt[i];
+        tt=tt+wt;
+    }
+    cout<<"Average waiting time : "<<(tt-wt)/float(n);
     return 0;
 }
